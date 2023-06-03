9fb4612368a1b20d5371b6a0750b2bf16ad36e79
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-03 13:56:20 +0900

    インデント除去される。終了文字列よりインデントが前に来ている行があるとエラーを返す。

--- web/type/heredoc.php
+++ web/type/heredoc.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 $str = 'PHP (PHP:Hypertext Preprocessor)';
-$msg = <<<'EOD'
-{$str}は、サーバーサイドで動作する簡易なスクリプト言語です。
-まずは、本書でじっくり基礎固めをしましょう。<br />
-"Let's start, everyone!!"
+	$msg = <<<'EOD'
+	{$str}は、サーバーサイドで動作する簡易なスクリプト言語です。
+	まずは、本書でじっくり基礎固めをしましょう。<br />
+	"Let's start, everyone!!"
 EOD;
 
 print $msg;
