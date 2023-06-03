9fbfd04a374ee076c7cd42673fbce18cd7796515
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-03 15:12:56 +0900

    preタグを使って見やすく

--- web/type/bracket.php
+++ web/type/bracket.php
@@ -1,3 +1,4 @@
+<pre>
 <?php 
 $data[0] = '山田';
 $data[1] = '掛谷';
@@ -5,6 +6,4 @@ $data[2] = '日尾';
 $data[3] = '本多';
 $data[1] = '薄井';
 $data[] = '矢吹';
-print $data[4];
-print '<br>';
 print_r($data);
