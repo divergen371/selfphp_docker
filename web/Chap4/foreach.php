0dd9ccc4eaaf59e4ef9beb57f3cfbaf630247c58
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-08 16:28:10 +0900

    foreachループ

---
+++ web/Chap4/foreach.php
@@ -0,0 +1,6 @@
+<?php
+
+$data = [1, 2, 3, 4, 5, 6];
+foreach ($data as $value) {
+	print $value;
+}
---
+++ web/Chap4/foreach_assoc.php
@@ -0,0 +1,6 @@
+<?php
+$data = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
+
+foreach ($data as $key => $value) {
+	print "{$key}, {$value}<br>";
+}
