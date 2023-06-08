4ae0e38f231c7c6261ac65afb34018b25f5ef850
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-08 18:15:46 +0900

    文字列の大文字小文字変換

---
+++ web/Chap5/mb_convert_case.php
@@ -0,0 +1,12 @@
+<?php
+
+$data1 = 'Wings project';
+$data2 = 'WINGSプロジェクト';
+$data3 = 'Fußball';
+
+print mb_convert_case($data1, MB_CASE_UPPER);
+print mb_convert_case($data1, MB_CASE_LOWER);
+print mb_convert_case($data1, MB_CASE_TITLE);
+print mb_convert_case($data2, MB_CASE_LOWER);
+print mb_convert_case($data3, MB_CASE_UPPER);
+print mb_convert_case($data3, MB_CASE_UPPER_SIMPLE);
